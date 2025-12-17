4346a525aa2c2106668a9d377814c7687b140298
Djomoro <cherifsylla21@gmail.com>
2025-12-17 08:06:54 +0100

    IDolomite

--- src/interfaces/IDolomite.sol
+++ src/interfaces/IDolomite.sol
@@ -58,3 +58,95 @@ interface IDolomiteMargin {
 
     function getMarketIdByTokenAddress(address token) external view returns (uint256);
 }
+
+interface IDepositWithdrawalRouter {
+    enum EventFlag {
+        None
+    }
+    
+    function depositWei(
+        uint256 isolationModeMarketId,
+        uint256 toAccountNumber,
+        uint256 marketId,
+        uint256 amountWei,
+        EventFlag eventFlag
+    ) external;
+    
+    function withdrawWei(
+        uint256 isolationModeMarketId,
+        uint256 fromAccountNumber,
+        uint256 marketId,
+        uint256 amountWei,
+        EventFlag eventFlag
+    ) external;
+}
+library AccountBalanceLib {
+    /// Checks that either BOTH, FROM, or TO accounts do not have negative balances
+    enum BalanceCheckFlag {
+        Both,
+        From,
+        To,
+        None
+    }
+}
+interface IBorrowPositionRouter {
+    enum BalanceCheckFlag {
+        Both,
+        From,
+        To,
+        None
+    }
+
+    function getAccountWei(
+        address owner,
+        uint256 accountNumber,
+        uint256 marketId
+    ) external view returns (int256);
+
+    function openBorrowPosition(
+        uint256 _fromAccountNumber,
+        uint256 _toAccountNumber,
+        uint256 _marketId,
+        uint256 _amount,
+        BalanceCheckFlag _balanceCheckFlag
+    ) external;
+
+    function transferBetweenAccounts(
+        uint256 _isolationModeMarketId,
+        uint256 _fromAccountNumber,
+        uint256 _toAccountNumber,
+        uint256 _marketId,
+        uint256 _amount,
+        BalanceCheckFlag _balanceCheckFlag
+    ) external;
+
+    /**
+     * @param _isolationModeMarketId    The market ID of the isolation mode token (0 if not using isolation mode)
+     * @param _fromAccountNumber        The account number containing repayment funds
+     * @param _borrowAccountNumber      The account number containing the borrow position
+     * @param _marketId                 The ID of the market to repay
+     * @param _balanceCheckFlag         Flag indicating how to validate account balances
+     */
+    function repayAllForBorrowPosition(
+        uint256 _isolationModeMarketId,
+        uint256 _fromAccountNumber,
+        uint256 _borrowAccountNumber,
+        uint256 _marketId,
+        AccountBalanceLib.BalanceCheckFlag _balanceCheckFlag
+    ) external;
+    
+    /**
+     * @param _isolationModeMarketId  The market ID of the isolation mode token
+     *                                (0 if not using isolation mode)
+     * @param _borrowAccountNumber    The account number containing the borrow position
+     * @param _toAccountNumber        The account number to send remaining collateral to
+     * @param _collateralMarketIds    Array of market IDs for collateral to be returned
+     */
+    function closeBorrowPosition(
+        uint256 _isolationModeMarketId,
+        uint256 _borrowAccountNumber,
+        uint256 _toAccountNumber,
+        uint256[] calldata _collateralMarketIds
+    ) external;
+
+}
